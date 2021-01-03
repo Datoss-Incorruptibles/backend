a0633167cdf1992ea99cd867c0bff19a31c83482
Lout33 <sssimple.33@gmail.com>
2021-01-03 12:12:20 -0500

    add service  partidos politicos

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Proceso, Cargo
+from .models import Proceso, Cargo, PartidoPolitico
 
 class ProcesoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -12,3 +12,9 @@ class CargoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Cargo
         fields = ('cargo', 'estado')
+
+class PartidoPoliticoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = PartidoPolitico
+        fields = ('nombre', 'fundacion_anio','ruta_archivo')
+
