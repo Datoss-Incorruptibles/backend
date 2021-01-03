d778ee025e74e5646b2d554e655da664ef79403a
Lout33 <sssimple.33@gmail.com>
2021-01-03 18:54:28 -0500

    create 3 models

--- api/models.py
+++ api/models.py
@@ -27,10 +27,13 @@ class Cargo(models.Model):
 
 
 
-class PartidoPolitico(models.Model):
+class OrganizacionPolitica(models.Model):
     nombre = models.CharField(max_length=150)
     fundacion_anio = models.IntegerField()
+    estado =  models.IntegerField()
+    descripcion = models.CharField(max_length=500)
     ruta_archivo = models.CharField(max_length=500)
+    jne_idorganizacionpolitica = models.IntegerField()
     fecha_registro = models.DateTimeField(default=datetime.now, blank=True)
     fecha_modificacion = models.DateTimeField(null=True)
 
@@ -39,3 +42,43 @@ class PartidoPolitico(models.Model):
     def __str__(self):
         return self.organizacion_politica
 
+class Indicador(models.Model):
+    nombre = models.CharField(max_length=150)
+    alerta = models.IntegerField()
+    estado = models.IntegerField()
+    fecha_registro = models.DateTimeField(default=datetime.now, blank=True)
+    fecha_modificacion = models.DateTimeField(null=True)
+    class Meta:
+        db_table = "_indicador"
+    def __str__(self):
+        return self._indicador
+
+
+class IndicadorCategoria(models.Model):
+    indicador_id = models.IntegerField()
+    nombre = models.CharField(max_length=150)
+    alerta = models.IntegerField()
+    estado =  models.IntegerField()
+    fecha_registro = models.DateTimeField(default=datetime.now, blank=True)
+    fecha_modificacion = models.DateTimeField(null=True)
+    class Meta:
+        db_table = "_indicador_categoria"
+    def __str__(self):
+        return self._indicador_categoria
+
+
+
+class IndicadorCategoriaOrganizacion(models.Model):
+    indicador_id = models.IntegerField()
+    indicador_categoria_id = models.IntegerField()
+    organizacion_id  = models.IntegerField()
+    cantidad =  models.IntegerField()
+    porcentaje  = models.FloatField()
+    alerta =  models.IntegerField()
+    estado =  models.IntegerField()
+    fecha_registro = models.DateTimeField(default=datetime.now, blank=True)
+    fecha_modificacion = models.DateTimeField(null=True)
+    class Meta:
+        db_table = "_indicador_categoria_organizacion"
+    def __str__(self):
+        return self._indicador_categoria_organizacion
