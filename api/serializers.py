1ef584bbff236bb1bcfbb4c4fcd8bbb75b1c041e
Diego Machaca <diego.machaca@gmail.com>
2021-01-05 00:32:09 -0500

    feat(organizacion politica): serializacion

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Proceso, Cargo, OrganizacionPolitica, IndicadorCategoriaOrganizacion ,IndicadorCategoria
+from .models import Proceso, Cargo, OrganizacionPolitica, IndicadorCategoriaOrganizacion ,IndicadorCategoria, Indicador
 
 class ProcesoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -14,28 +14,31 @@ class CargoSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('cargo', 'estado')
 
 
+class IndicadorSerializer(serializers.ModelSerializer):
 
+    class Meta:
+        model = Indicador
+        fields = ('nombre','alerta','estado')
 class IndicadorCategoriaSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = IndicadorCategoria
-        fields = ('indicador','nombre','alerta','estado','order','fecha_registro','fecha_modificacion')
-
-
+        fields = ('indicador','nombre','alerta','estado','order')
 
 class IndicadorCategoriaOrganizacionSerializer(serializers.ModelSerializer):
-
-    indicadorescategoria = IndicadorCategoriaSerializer(many=True, read_only=True)
+    indicador_categoria_nombre = serializers.CharField(source='indicador_categoria', read_only=True)
+    indicador_nombre = serializers.CharField(source='indicador', read_only=True)
     class Meta:
         model = IndicadorCategoriaOrganizacion
-        fields = ('indicador','indicador_categoria','organizacion','cantidad','porcentaje','alerta','estado','fecha_registro','fecha_modificacion','indicadorescategoria')
+        fields = ('indicador_categoria_nombre','indicador_categoria','indicador_nombre','indicador',
+                'cantidad','porcentaje','alerta','estado')
 
 
 class OrganizacionPoliticaSerializer(serializers.ModelSerializer):
-
+    id = serializers
     # tracks = serializers.StringRelatedField(many=True)
     indicadorescategoriaorg = IndicadorCategoriaOrganizacionSerializer(many=True, read_only=True)
     class Meta:
         model = OrganizacionPolitica
-        fields = ('nombre','fundacion_anio', 'estado','descripcion','ruta_archivo','jne_idorganizacionpolitica','fecha_registro','fecha_modificacion','indicadorescategoriaorg')
+        fields = ('id','nombre','fundacion_anio', 'estado','descripcion','ruta_archivo','jne_idorganizacionpolitica','indicadorescategoriaorg')
 
