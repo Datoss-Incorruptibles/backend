5da1f3217432d82b29b36cc7957abc0b4f80129e
Lout33 <sssimple.33@gmail.com>
2021-01-08 20:56:55 -0500

    activate all origin cors

--- backend/settings.py
+++ backend/settings.py
@@ -27,9 +27,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -38,6 +40,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS= True
 
 ROOT_URLCONF = 'backend.urls'
 
