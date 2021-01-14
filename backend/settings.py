1c74e7705d3d56ae68b1ab01c6c53a9f3063db3b
Diego Machaca <diego.machaca@gmail.com>
2021-01-14 00:06:06 -0500

    chore(static): assets in serverless

--- backend/settings.py
+++ backend/settings.py
@@ -115,3 +115,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT  = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
