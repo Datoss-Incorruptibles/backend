d369f7749afe37e19e17c9533df4eca03d86aa42
Lout33 <sssimple.33@gmail.com>
2021-01-18 02:20:21 -0500

    improve pagination and create new api

--- api/urls.py
+++ api/urls.py
@@ -8,6 +8,7 @@ router.register(r'cargos', views.CargoViewSet)
 router.register(r'organizacionpolitica', views.OrganizacionPoliticaViewSet)
 router.register(r'candidato', views.CandidatoViewSet)
 router.register(r'ubigeo', views.UbigeoViewSet)
+router.register(r'orgpolcombo', views.OrgPolComboViewSet,basename='orgpolcombo')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
