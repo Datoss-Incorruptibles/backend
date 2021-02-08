39eeb51a31a9e1cb03bcdae0cda4b0fec8407592
Diego Machaca <diego.machaca@gmail.com>
2021-02-07 23:40:10 -0500

    feat(candidatos): administrator medios

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CandidatoMedio
+
 
 # Register your models here.
+class CandidatoMedioAdmin(admin.ModelAdmin):
+    fields = ('titulo', 'url','medio','tipo','fecha','jne_idhojavida')
+
+admin.site.register(CandidatoMedio, CandidatoMedioAdmin)
--- backend/settings.py
+++ backend/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'api',
     'corsheaders',
     'django_filters',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -119,12 +120,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+USE_S3 = os.environ.get('USE_S3',False)
 
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.environ.get('SLS_ACCESS')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('SLS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('SLS_BUCKET')
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    AWS_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+else:
+    STATIC_URL = '/staticfiles/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT  = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
